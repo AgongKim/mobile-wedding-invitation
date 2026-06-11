0ad27dd346606f4d8164bd159be73a6764c0d3fb
thkim <thkim@babitalk.com>
2026-06-11 21:55:36 +0900

    구글캘린더 연동 추가

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -5,16 +5,27 @@ import RoundButton from '@/components/RoundButton.tsx';
 import { Caption, Paragraph } from '@/components/Text.tsx';
 
 const Invitation = () => {
-  const { greeting } = data;
+  const { greeting, mapInfo } = data;
+
+  // 2026년 9월 19일 (토) 11:00 ~ 13:00 KST(UTC+9) -> UTC 02:00 ~ 04:00
+  const calendarUrl = `https://calendar.google.com/calendar/render?${new URLSearchParams(
+    {
+      action: 'TEMPLATE',
+      text: `${greeting.host.groom.name} ❤ ${greeting.host.bride.name} 결혼식`,
+      dates: '20260919T020000Z/20260919T040000Z',
+      details: greeting.message,
+      location: mapInfo.address1,
+    },
+  ).toString()}`;
+
   return (
     <InvitationWrapper>
       <Paragraph>{greeting.message}</Paragraph>
       <Host />
       <Caption textAlign={'center'}>{greeting.eventDetail}</Caption>
-      {/* TODO: 구글캘린더 추가하기 기능을 넣는다면 링크 수정 */}
       <RoundButton
         target="_blank"
-        href=""
+        href={calendarUrl}
         rel="noreferrer">
         구글 캘린더 추가하기
       </RoundButton>
