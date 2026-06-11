17076879cb002c16814c11ccb294037a9d78ebf2
thkim <thkim@babitalk.com>
2026-06-11 21:54:30 +0900

    메인 이미지 컨테이너 변경 및 이미지 압축

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,38 +1,83 @@
 import styled from '@emotion/styled';
-import data from 'data.json';
-import mainImg from '@/assets/images/00129.jpg'
+import mainImg from '@/assets/images/00129.jpg';
 
 const Main = () => {
-  const { greeting } = data;
   return (
-    <div>
-      <MainImg src={mainImg} />
-      <MainTitle>{greeting.title}</MainTitle>
-      <SubTitle>{greeting.eventDetail}</SubTitle>
-    </div>
+    <Hero>
+      <Announcement>
+        <CoupleName>Taehyun &amp; Doyeon</CoupleName>
+        <Subtitle>| WE ARE GETTING MARRIED |</Subtitle>
+        <EventInfo>
+          <b>2026. 09. 19. SAT AM 11:00</b>
+          <br />
+          더시그너스 웨딩 수원 컨벤션홀
+        </EventInfo>
+      </Announcement>
+    </Hero>
   );
 };
 
 export default Main;
 
-const MainImg = styled.img`
-  border-radius: 200px 200px 0 0;
-  width: 90%;
-  max-width: 450px;
-  padding-top: 20px;
+const Hero = styled.div`
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  padding-bottom: 60px;
+  box-sizing: border-box;
+  width: calc(100% + 60px);
+  height: 100vh;
+  min-height: 480px;
+  margin: -30px -30px 0;
+  background:
+    linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url(${mainImg}) no-repeat center center;
+  background-size: cover;
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(245, 138, 119, 0.2);
+  }
 `;
 
-const MainTitle = styled.p`
-  font-family: HSSanTokki20-Regular, serif;
-  font-size: 2rem;
-  color: #2F2120;
-  line-height: 120%;
-  white-space: pre-line;
+const Announcement = styled.div`
+  position: relative;
+  z-index: 1;
+  max-width: 600px;
+  padding: 0 20px;
+  text-align: center;
+  color: #fff;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.4);
+  letter-spacing: 2px;
 `;
 
-const SubTitle = styled.p`
-  font-size: 1.1rem;
-  color: #2F2120;
-  line-height: 140%;
-  white-space: pre-line;
+const CoupleName = styled.p`
+  font-family: 'Great Vibes', cursive;
+  font-size: 70px;
+  margin: 0;
+
+  @media (max-width: 576px) {
+    font-size: 50px;
+  }
+`;
+
+const Subtitle = styled.p`
+  font-family: 'Gugi', cursive;
+  font-size: 1rem;
+  margin: 2rem 0 0;
+`;
+
+const EventInfo = styled.p`
+  font-family: 'Noto Serif KR', serif;
+  font-size: 1rem;
+  letter-spacing: -1px;
+  margin: 2rem 0 0;
+  line-height: 1.6;
+
+  b {
+    font-size: 1.4em;
+  }
 `;
